0fbb049ee34f25c70e62f59ca938b7ff97ad5c9c
AssanssinHotstrip <670204191@qq.com>
2018-12-31 11:53:57 +0800

    spiderassassin base

--- Scrapy/SpiderSelfDdefined/SpiderAssassin/core/spider.py
+++ Scrapy/SpiderSelfDdefined/SpiderAssassin/core/spider.py
@@ -9,13 +9,13 @@ class Spider(object):
     """框架提供的爬虫类原型"""
 
     # 默认到百度
-    start_url = "http://www.baidu.com"
+    start_url = "http://www.baidu.com/"
 
     def start_request(self):
-        # 返回第一个请求给引擎--->调度器
+        # 返回第一个请求给引擎--->调度
         return Request(self.start_url)
 
     def parse(self, response):
-        data = {"url" : response.url, "status" : response.status}
+        data = {"url" : response.url, "status" : response.status, "content" : response.body}
         return Item(data)
 
---
+++ Scrapy/SpiderSelfDdefined/mian.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+
+
+from SpiderAssassin.core.engine import Engine
+
+
+def main():
+    engine = Engine()
+    engine.start()
+
+if __name__ == '__main__':
+    main()
